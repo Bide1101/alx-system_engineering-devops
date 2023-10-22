d58a49d19d81bf3981c90b8211abf68c51cf45d6
Bide1101 <123000536+Bide1101@users.noreply.github.com>
2023-10-22 05:50:14 -0700

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,8 +0,0 @@
-# This reates a file in /tmp.
-file { '/tmp/school':
-  ensure  => 'file',
-  content => 'I love Puppet',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,5 +0,0 @@
-# This script kills a process named killmenow.
-exec { 'kill':,
-  command => 'pkill killmenow',
-  path    => ['/usr/bin', '/usr/sbin'],
-}
